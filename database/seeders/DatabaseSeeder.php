5087331dc970b51c2a552945a22d9855ce80475b
GunelMehnetli <gunel.mehnetli@gmail.com>
2023-10-18 17:35:17 +0400

    question migrate create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,11 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {       
+    {
         $this->call([
             UserSeeder::class,
-            QuizSeeder::class
+            QuizSeeder::class,
+            QuestionSeed::class
         ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
